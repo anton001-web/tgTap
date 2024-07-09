33a5e6f8e483505c28d90a8179af998e8b565124
Anton <you@example.com>
2024-07-10 00:46:08 +0300

    g

--- src/components/DesktopMobile/DesktopMobile.tsx
+++ src/components/DesktopMobile/DesktopMobile.tsx
@@ -1,6 +1,6 @@
 import s from './styles.module.scss'
 import {FC} from 'react'
-import Code from '../../assets/images/qrCode.svg?react'
+import code from '../../assets/images/qrCode.png'
 
 interface DesktopMobileProps {}
 
@@ -8,7 +8,7 @@ export const DesktopMobile:FC<DesktopMobileProps> = () => {
     return (
         <div className={s.modalWrap}>
             <div className={s.modalQrWrap}>
-                <Code className={s.CodeImg} />
+                <img src={code} className={s.CodeImg} />
             </div>
             <div className={s.modalText}>
                 <span>Open app on your mobile</span>
